1ed1412f4c2f486652f2f84a90b9153774586eb8
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-03-08 11:19:39 +0100

    Add Github repos visualization

---
+++ github/python_repos.py
@@ -0,0 +1,57 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+
+# Process results.
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_links, repo_stars, repo_labels = [], [], []
+
+for repo_dict in repo_dicts:
+    name = repo_dict['name']
+    url = repo_dict['html_url']
+    link = f"<a href='{url}'>{name}</a>"
+    star_count = repo_dict['stargazers_count']
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    repo_links.append(link)
+    repo_stars.append(star_count)
+    repo_labels.append(label)
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': repo_stars,
+    'hovertext': repo_labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-starred Python projects on Github',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
